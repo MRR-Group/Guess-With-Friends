7eb45920659468b1b3f2642ca874425ab663d9c5
Kacper Walenga <87613926+KacperWalenga@users.noreply.github.com>
2026-03-08 00:48:19 +0100

    theme config

---
+++ lib/config/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const Gradient backgroundColor = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  stops: [0.3, 0.84],
+  colors: [Color(0xFF121212), Color(0xFF000000)],
+);
+const Color backgroundLightColor = Color(0xFF212121);
+
+const Color textColor = Color(0xFFEAEAEA);
+const Color textDisabledColor = Color(0xFFD7D7D7);
+const Color textColorLight = Color(0xFFF1F7F6);
+const Color secondaryTextColor = Color(0xFF339696);
+
+const Color primaryColor = Color(0xFF339696);
+const Color primaryHoverColor = Color(0xFF47C5C5);
+const Color secondaryColor = Color(0xFFEAEAEA);
+const Color secondaryHoverColor = Color(0xFFFFFFFF);
+
+const Color skipColor = Color(0xFF4D4D4D);
+const Color errorColor = Color(0xFFD65F2F);
+const Color successColor = Color(0xFF339696);
+const Color halfSuccessColor = Color(0xFFBEAF27);
---
+++ lib/config/theme.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+ThemeData theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+  scaffoldBackgroundColor: backgroundColor.colors.first,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(color: textColor),
+    bodyMedium: TextStyle(color: textColor),
+    bodySmall: TextStyle(color: textColor),
+    displayLarge: TextStyle(color: textColor),
+    displayMedium: TextStyle(color: textColor),
+    displaySmall: TextStyle(color: textColor),
+    titleLarge: TextStyle(color: textColor),
+    titleMedium: TextStyle(color: textColor),
+    titleSmall: TextStyle(color: textColor),
+    headlineLarge: TextStyle(color: textColor),
+    headlineMedium: TextStyle(color: textColor),
+    headlineSmall: TextStyle(color: textColor),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: WidgetStateProperty.resolveWith<Color>((states) {
+        if (states.contains(WidgetState.disabled)) {
+          return textDisabledColor;
+        }
+        return textColor;
+      }),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: WidgetStateProperty.resolveWith<Color>((states) {
+        if (states.contains(WidgetState.disabled)) {
+          return textDisabledColor;
+        }
+        return textColor;
+      }),
+    ),
+  ),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    iconTheme: IconThemeData(
+      color: textColor,
+    ),
+  ),
+  cardTheme: CardThemeData(
+    margin: EdgeInsets.zero,
+    color: backgroundLightColor,
+    shadowColor: Colors.grey,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: BorderSide(color: Colors.transparent, width: 0)
+    ),
+  ),
+  snackBarTheme: const SnackBarThemeData(
+    insetPadding: EdgeInsets.zero,
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: backgroundLightColor,
+    hintStyle: const TextStyle(color: textDisabledColor),
+    labelStyle: const TextStyle(color: textDisabledColor),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide.none,
+    ),
+  ),
+  popupMenuTheme: PopupMenuThemeData(
+    color: backgroundLightColor,
+    shape: RoundedRectangleBorder(
+      side: const BorderSide(
+        color: primaryColor,
+      ),
+      borderRadius: BorderRadius.circular(12),
+    ),
+    elevation: 8,
+  ),
+);
